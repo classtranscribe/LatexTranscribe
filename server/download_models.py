66b079268eaebcc65ade5bcd2da53a06348bdff8
LouisAsanaka <25756888+LouisAsanaka@users.noreply.github.com>
2025-02-15 15:12:16 -0800

    Share model download script

--- server/download_models.py
+++ server/download_models.py
@@ -1,10 +1,18 @@
 # /// script
 # requires-python = ">=3.11"
 # dependencies = [
-#     "huggingface-hub",
+#     "huggingface-hub[hf-transfer]",
 # ]
 # ///
 from huggingface_hub import snapshot_download
 
-snapshot_download(repo_id='opendatalab/pdf-extract-kit-1.0', local_dir='./',
-                  allow_patterns=['models/Layout/*', 'models/MFD/*', 'models/MFR/*', 'models/TabRec/*'])
+snapshot_download(
+    repo_id="opendatalab/pdf-extract-kit-1.0",
+    local_dir="./",
+    allow_patterns=[
+        "models/Layout/*",
+        "models/MFD/*",
+        "models/MFR/*",
+        "models/TabRec/*",
+    ],
+)
